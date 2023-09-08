e359d7861fff0e8a206b8521d02a4253a17a292a
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-09-08 19:33:43 -0400

    used next method to iterate through object

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 // iterator - object with a next method
 
+// const company = {
+//     currEmployee: 0,
+//     employees: ['Donna', 'Thelma', 'Laura'],
+//     next() { //makes company object iterable. returns an object with a value property and a done property
+//         if (this.currEmployee >= this.employees.length){
+//             return {value: this.currEmployee, done: true} // signals when out of employees. sets done to true
+//         }
+//         const returnValue =  {value: this.employees[this.currEmployee], done: false};
+//         this.currEmployee++;
+//         return returnValue;
+//     }
+// }
+
+// console.log(company.next())
+// console.log(company.next())
+// console.log(company.next())
+// console.log(company.next())
+
 const company = {
     currEmployee: 0,
     employees: ['Donna', 'Thelma', 'Laura'],
-    next() {
+    next() { //makes company object iterable. returns an object with a value property and a done property
         if (this.currEmployee >= this.employees.length){
-            return {value: this.currEmployee, done: true}
+            return {
+                value: this.currEmployee, 
+                done: true // signals when out of employees. sets done to true
+            } 
         }
-        const returnValue =  {value: this.employees[this.currEmployee], done: false};
+        const returnValue =  {
+            value: this.employees[this.currEmployee], 
+            done: false // sinals that their are still employees. sets done to false
+        }; 
         this.currEmployee++;
+        // console.log(returnValue)
         return returnValue;
     }
 }
 
-console.log(company.next())
-console.log(company.next())
-console.log(company.next())
-console.log(company.next())
+let employee = company.next()
+
+while(!employee.done) { // negates done: false to !false returning true enter loop and negates done: true to !true returning false to not enter the loop
+    console.log(!employee.done)
+    console.log(company.currEmployee, employee.value, employee);
+    employee = company.next()
+}
