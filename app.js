8bd40d12097c4e4d1c5bee3c7ecc4708510e786e
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-09-08 21:20:37 -0400

    array example to show arryas use the Symbol.iterator and the next method implicitly for iteration

--- app.js
+++ app.js
@@ -181,6 +181,47 @@
 
 // Example 6 (with Symbol.iterator and for of loop)
 
+// const company = {
+//     currEmployee: 0,
+//     employees: ['Donna', 'Thelma', 'Laura'],
+//     // next() { //makes company object iterable. returns an object with a value property and a done property
+//     //     if (this.currEmployee >= this.employees.length){
+//     //         return {
+//     //             value: this.currEmployee, 
+//     //             done: true // signals when out of employees. sets done to true
+//     //         } 
+//     //     }
+//     //     const returnValue =  {
+//     //         value: this.employees[this.currEmployee], 
+//     //         done: false // sinals that their are still employees. sets done to false
+//     //     }; 
+//     //     this.currEmployee++;
+//     //     // console.log(returnValue)
+//     //     return returnValue;
+//     // },
+//     // Symbol.iterator value has to be an iterator (object with a next method)
+//     [Symbol.iterator]: function* emplyoyeeGenerator() { // function* is a generator. it builds an iterator object (has a next methood)
+//             //let employee = company.next() //while loop adds looping logic
+//             //  while(!employee.done) { // negates done: false to !false returning true to enter the loop and negates done: true to !true returning false to not enter the loop
+//             //         yield employee.value;
+//             //         employee = company.next()
+//             // }
+//         let currentEmployee = 0;
+//         while(currentEmployee < this.employees.length) {
+//             yield this.employees[currentEmployee]; // when yield is encountered JS saves the current state of exe and the next time the next() is called it continues from that point
+//             currentEmployee++;
+//         }
+//     }
+// }
+
+// for (const employee of company) { // for of loop iterates therough object(company) being looped through and searches for Symbol.iterator then executes the function if finds which should be a Generator. Executes next as long as done is not true
+//     console.log(employee);
+// }
+// let ride = [...company]// spread operator looks for Symbol.iterator then goes through all of the values and adds them as elements of array
+// console.log(ride) 
+
+// Example 7 ()
+
 const company = {
     currEmployee: 0,
     employees: ['Donna', 'Thelma', 'Laura'],
@@ -214,10 +255,11 @@ const company = {
     }
 }
 
-
-
 for (const employee of company) { // for of loop iterates therough object(company) being looped through and searches for Symbol.iterator then executes the function if finds which should be a Generator. Executes next as long as done is not true
     console.log(employee);
 }
 let ride = [...company]// spread operator looks for Symbol.iterator then goes through all of the values and adds them as elements of array
 console.log(ride) 
+
+const djs = ['Tray', 'Swift'];//array is simply an object with the Symbol(Symbol.iterator): ƒ values(). which is a generator function that yields an iterator object with next(). The length property of th array in tandem with the Genrator help control what values are return from the array
+console.log(djs)
